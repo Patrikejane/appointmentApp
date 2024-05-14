8f9690f63d9d4ed3484a8f5b3f42e54e54a1846a
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-15 08:58:14 +0900

    Update appointment-list.component.ts
    Added ng on init

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,47 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Appointment } from '../models/appointment';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
-  styleUrls: ['./appointment-list.component.css']
+  styleUrls: ['./appointment-list.component.css'],
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent  implements OnInit {
 
-    appointments:Appointment[] = [{
-      id : Date.now(),
-      title: "Take a Dog to walk",
-      date: new Date('2024-03-25')
+
+  appointments: Appointment[] = [
+    {
+      id: Date.now(),
+      title: 'Take a Dog to walk',
+      date: new Date('2024-03-25'),
     },
     {
-      id : Date.now(),
-      title: "Take a Dog to walk",
-      date: new Date('2024-03-25')
+      id: Date.now(),
+      title: 'Take a Dog to walk',
+      date: new Date('2024-03-25'),
+    },
+  ];
+
+  appointmentTitle: string = '';
+  appointmentDate: Date = new Date();
+
+
+
+  ngOnInit(): void {
+
+    let savedAppointments = localStorage.getItem("appointment");
+    this.appointments = savedAppointments ? JSON.parse(savedAppointments):[];
+    
+  }
+
+
+
+
+  addAppointment() {
+    if (this.appointmentTitle.trim().length && this.appointmentDate) {
+      let newAppontment = {
+        id: Date.now(),
+        title: this.appointmentTitle,
+        date: this.appointmentDate,
+      };
+
+      this.appointments.push(newAppontment);
+
+      this.appointmentTitle = '';
+      this.appointmentDate = new Date();
+
+      localStorage.setItem("appointment",JSON.stringify(this.appointments));
+      // alert(this.appointments.length);
+      // console.log(appointments.length);
     }
-    ]
+  }
 
-    appointmentTitle:string = "";
-    appointmentDate:Date = new Date();
+  deleteAppointment(index: number) {
+    this.appointments.splice(index, 1);
 
-
-    addAppointment(){
-      if(this.appointmentTitle.trim().length && this.appointmentDate){
-          let newAppontment = {
-            id : Date.now(),
-            title: this.appointmentTitle,
-            date: this.appointmentDate
-          }
-
-          this.appointments.push(newAppontment);
-
-          this.appointmentTitle = "";
-          this.appointmentDate = new Date();
-          alert(this.appointments.length);
-          // console.log(appointments.length);
-      }
-
-
-    }
-
-
+    localStorage.setItem("appointment",JSON.stringify(this.appointments));
+  }
 }
